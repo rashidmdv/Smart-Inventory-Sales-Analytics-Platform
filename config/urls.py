950f60e46e1768cb572646bdac06f6e76740291a
Muhammmed Rashid V <mhdrashiv@gmail.com>
2025-12-22 22:29:29 +0530

    settings and urls updated

--- config/urls.py
+++ config/urls.py
@@ -32,6 +32,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/accounts/", include("apps.accounts.urls")),
+    path("api/inventory/", include("apps.inventory.urls")),
 
     # API Documentation
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
