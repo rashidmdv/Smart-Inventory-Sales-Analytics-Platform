aa3b5b92f4cd9baec142271e97b6ff7295bdec4b
Muhammmed Rashid V <mhdrashiv@gmail.com>
2025-12-22 22:25:08 +0530

    inventry models created

---
+++ apps/inventory/models/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    sku = models.CharField(max_length=50, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2 ,null=True, blank=True)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
+    tax_percentage = models.IntegerField(max_length=3,default=0)
+
+
+class Inventory(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+
+class StockTransaction(models.Model):
+    IN = "IN"
+    OUT = "OUT"
+
+    TRANSACTION_TYPE = [
+        (IN, "Purchase / Stock In"),
+        (OUT, "Sale / Stock Out"),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=3, choices=TRANSACTION_TYPE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
